5b5027026b10ce44bf05484d74cfc5219bf615ff
sangrit-siit <Kittikom.San@dome.tu.ac.th>
2020-02-29 21:13:29 +0700

    fix push-to-mqtt with mbed-mqtt

--- push-to-mqtt/main.cpp
+++ push-to-mqtt/main.cpp
@@ -1,5 +1,4 @@
 #include <mbed.h>
-#include "https_request.h"
 #include <MQTTClientMbedOs.h>
 
 DigitalOut led(LED1);
@@ -8,46 +7,25 @@ Serial pc(USBTX, USBRX);
 InterruptIn button(USER_BUTTON);
 
 WiFiInterface *wifi;
+
 TCPSocket socket;
+
 MQTTClient client(&socket);
 
 Thread thread;
 EventQueue queue(5 * EVENTS_EVENT_SIZE);
 
-const char* hostname = "https://api.netpie.io/v2/device/message";
-//const char* hostname = "mqtt.netpie.io"; 
+const char* hostname = "mqtt.netpie.io"; 
 uint16_t  port = 1883;
 
 const char* mqtt_client = "4665fab9-4827-40de-a1a6-36e538463bc4";
 const char* mqtt_username = "CXhbMLgUwHFZWKdt77AHEVAgio42f3k7";
 const char* mqtt_password = "zdsj67RATuG~~QftY+Y8XY_TS2XCtmEE";
 
-const char* topic = "@msg/ICT710";
-
+const char* topic = "@msg/ict710";
 MQTT::Message message;
-const char SSL_CA_PEM[] ="-----BEGIN CERTIFICATE-----\n"
-"MIIDdTCCAl2gAwIBAgILBAAAAAABFUtaw5QwDQYJKoZIhvcNAQEFBQAwVzELMAkG\n"
-"A1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNVBAsTB1Jv\n"
-"b3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw05ODA5MDExMjAw\n"
-"MDBaFw0yODAxMjgxMjAwMDBaMFcxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9i\n"
-"YWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290IENBMRswGQYDVQQDExJHbG9iYWxT\n"
-"aWduIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDaDuaZ\n"
-"jc6j40+Kfvvxi4Mla+pIH/EqsLmVEQS98GPR4mdmzxzdzxtIK+6NiY6arymAZavp\n"
-"xy0Sy6scTHAHoT0KMM0VjU/43dSMUBUc71DuxC73/OlS8pF94G3VNTCOXkNz8kHp\n"
-"1Wrjsok6Vjk4bwY8iGlbKk3Fp1S4bInMm/k8yuX9ifUSPJJ4ltbcdG6TRGHRjcdG\n"
-"snUOhugZitVtbNV4FpWi6cgKOOvyJBNPc1STE4U6G7weNLWLBYy5d4ux2x8gkasJ\n"
-"U26Qzns3dLlwR5EiUWMWea6xrkEmCMgZK9FGqkjWZCrXgzT/LCrBbBlDSgeF59N8\n"
-"9iFo7+ryUp9/k5DPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8E\n"
-"BTADAQH/MB0GA1UdDgQWBBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0B\n"
-"AQUFAAOCAQEA1nPnfE920I2/7LqivjTFKDK1fPxsnCwrvQmeU79rXqoRSLblCKOz\n"
-"yj1hTdNGCbM+w6DjY1Ub8rrvrTnhQ7k4o+YviiY776BQVvnGCv04zcQLcFGUl5gE\n"
-"38NflNUVyRRBnMRddWQVDf9VMOyGj/8N7yy5Y0b2qvzfvGn9LhJIZJrglfCm7ymP\n"
-"AbEVtQwdpf5pLGkkeB6zpxxxYu7KyJesF12KwvhHhm4qxFYxldBniYUr+WymXUad\n"
-"DKqC5JlR3XC321Y9YeRq4VzW9v493kHMB65jUr9TU/Qr6cf9tveCX4XSQRjbgbME\n"
-"HMUfpIBvFSDJ3gyICh3WZlXi/EjJKSZp4A==\n"
-"-----END CERTIFICATE-----\n";
 
-void  connect_wifi() {
+void pressed_handler(){
 
     wifi = WiFiInterface::get_default_instance();
 
@@ -69,46 +47,31 @@ void  connect_wifi() {
     printf("IP: %s\n\r", wifi->get_ip_address());
     printf("Netmask: %s\n\r", wifi->get_netmask());
     printf("Gateway: %s\n\r", wifi->get_gateway());
-    printf("RSSI: %d\n\n\r", wifi->get_rssi());   
-    //wifi->disconnect();
-    //pc.printf("\n\rDone\n\r");    
-}
+    printf("RSSI: %d\n\n\r", wifi->get_rssi());         
 
-void connect_mqtt() {
+    pc.printf("\nConnecting to broker...\n\r");
+    socket.open(wifi);
+    socket.connect(hostname, port);
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;       
+    data.MQTTVersion = 3;
+    data.clientID.cstring = (char*)mqtt_client;
+    data.username.cstring = (char*)mqtt_username;
+    client.connect(data);
+    pc.printf("Success\n\n\r");
     
-    char endpoint[128];
-    char auth[128];
+    char buf[128];
+    sprintf(buf, "DAMN  NETPIE!");
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
 
-    sprintf(endpoint, "%s/%s", hostname, topic);
-    sprintf(auth, "Device %s:%s", mqtt_client, mqtt_username);
-    
-    pc.printf("PUT %s\n\r", endpoint);
+    while(1){
+        client.publish(topic, message);
+        pc.printf("Published topic: %s\tmessage: %s\n\r", topic, buf);
+        ThisThread::sleep_for(1000);
+    }
 
-    HttpsRequest* request = new HttpsRequest(wifi, SSL_CA_PEM, HTTP_PUT, endpoint);
-
-    request->set_header("Authorization", auth);
-    request->set_header("Content-type", "text/plain");
-
-    const char* body = "Hello NETPIE2020";
- 
-    HttpResponse* response = request->send(body, strlen(body));
-
-    printf("status is %d\n\r", response->get_status_code());
-    printf("body is:\n%s\n\r", response->get_body());
-
-    delete request;// also clears out the response
-
-    wifi->disconnect();
-    pc.printf("\nDone\n\r");
-
-}
-
-
-void pressed_handler(){
-    // Connect to network interface
-    connect_wifi();
-    // Put a message to MQTT Broker
-    connect_mqtt();
 }
 
 int main() {
